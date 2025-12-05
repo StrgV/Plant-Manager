29028956683bd64986a1b85ce523452ccf0bbd73
StrgV <timporgertim@gmail.com>
2025-12-05 14:11:53 +0100

    Optimzie scaling for mobile

--- server/src/routes/home/+page.svelte
+++ server/src/routes/home/+page.svelte
@@ -8,7 +8,7 @@
       <a href="https://github.com/StrgV/Plant-Manager">
         <img src="github.png" alt="Github" style="background-color: white; border-radius: 50%" />
       </a>
-      <p>Check us out on <a href="https://www.youtube.com/@iotpflanzenmonitor">Github</a>!</p>
+      <p>Check us out on <a href="https://github.com/StrgV/Plant-Manager">Github</a>!</p>
     </div>
     <div>
       <a href="https://www.youtube.com/@iotpflanzenmonitor">
@@ -35,22 +35,26 @@
     flex-direction: column;
     justify-content: center;
     align-items: center;
-    margin: 10rem;
+    margin: 3rem;
+    font-size: clamp(1.5rem, 2vw + 0.5rem, 3rem);
   }
   #heading {
     display: flex;
     align-items: center;
+    font-size: clamp(2rem, 3vw, 4rem);
+    height: 15vw;
+    margin: 3rem;
   }
   #heading img {
-    height: 10rem;
-    margin: 2rem;
+    margin-inline: 2rem;
+    height: inherit;
   }
   div img {
-    height: 20rem;
+    height: 23vw;
     filter: drop-shadow(0px 0px 30px rgba(0, 0, 0, 0.5));
   }
   h1 {
-    font-size: 4rem;
+    font-size: inherit;
     font-family:
       system-ui,
       -apple-system,
@@ -65,7 +69,7 @@
       sans-serif;
   }
   p {
-    font-size: 2rem;
+    font-size: inherit;
     font-family:
       system-ui,
       -apple-system,
@@ -78,5 +82,19 @@
       'Open Sans',
       'Helvetica Neue',
       sans-serif;
+  }
+
+  @media (max-width: 500px) {
+    #links {
+      display: flex;
+      flex-direction: column;
+    }
+    #links div {
+      display: flex;
+      flex-direction: column;
+      justify-content: center;
+      align-items: center;
+      margin: 1rem;
+    }
   }
 </style>
